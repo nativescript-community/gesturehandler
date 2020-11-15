e1b0689d43e78ae55aa31e103cb975d3d63c8bc5
Martin Guillon <martin.guillon@akylas.fr>
2020-11-15 16:24:03 +0100

    test: updated demo app

--- demo_vue/app/App.vue
+++ demo_vue/app/App.vue
@@ -1,15 +1,26 @@
 <template>
-    <Page @navigatedTo="onNavigatedTo">
+    <Page >
         <ActionBar title="GestureHandler Demo" />
 
-        <BottomSheetHolder ref="bottomSheetHolder" :peekerSteps="[70, 120, 400]" :scrollViewTag="12">
-            <GridLayout ref="testView" backgroundColor="white">
-                <MDButton ref="button" text="test" @tap="onButtonTap" verticalAlignment="top" />
-                <Label text="test label for gestures" @doubletap="printGestureResult" @swipe="printGestureResult" marginTop="100" verticalAlignment="top" />
-                <StackLayout ref="subView" width="50" height="50" backgroundColor="yellow" horizontalAlignment="center" verticalAlignment="center" :translateX="translateX" :translateY="translateY" />
-            </GridLayout>
-            <BottomSheet slot="bottomSheet" />
-        </BottomSheetHolder>
+        <GridLayout>
+            <CollectionView
+                ref="collectionView"
+                iosOverflowSafeArea="true"
+                :items="items"
+                itemIdGenerator="index"
+                colWidth="50%"
+                rowHeight="200"
+            >
+                <v-template>
+                    <GridLayout rows="*, auto" :backgroundColor="item.color" class="item">
+                        <StackLayout row="1">
+                            <Label row="1" :text="item.name" class="title" />
+                            <Label row="1" :text="item.color" class="subtitle" />
+                        </StackLayout>
+                    </GridLayout>
+                </v-template>
+            </CollectionView>
+        </GridLayout>
     </Page>
 </template>
 
@@ -19,114 +30,83 @@ import BaseVueComponent from './BaseVueComponent';
 import BottomSheetHolder from './BottomSheetHolder';
 import BottomSheet from './BottomSheet';
 import Component from 'vue-class-component';
-import { GestureState, GestureHandlerTouchEvent, GestureHandlerStateEvent, GestureStateEventData, GestureTouchEventData, HandlerType, Manager } from '@nativescript-community/gesturehandler';
+import {
+    FlingDirection,
+    FlingGestureHandler,
+    GestureHandlerStateEvent,
+    GestureState,
+    GestureStateEventData,
+    HandlerType,
+    Manager,
+    NativeViewGestureHandler,
+} from '@nativescript-community/gesturehandler';
 import { View } from '@nativescript/core';
 import { Provide } from 'vue-property-decorator';
+export const FLING_GESTURE_TAG = 12431;
+export const NATIVE_GESTURE_TAG = 12331;
 
 @Component({
     components: {
-        BottomSheetHolder,
-        BottomSheet
     }
 })
 export default class App extends BaseVueComponent {
-    // itemList = samples;
-    translateX = 0;
-    translateY = 0;
-    deltaX = 0;
-    deltaY = 0;
-    onNavigatedTo() {
-        // console.log('app', 'onNavigatedTo');
-        GC();
-    }
-
-    get bottomSheetHolder() {
-        return this.$refs.bottomSheetHolder as BottomSheetHolder;
-    }
-
-    // mListViewAllowScroll = true;
-    // get listViewAllowScroll() {
-    //      return this.mListViewAllowScroll;
-    // }
-    // onListViewAllowScrollChange(value) {
-    //     console.log('App set listViewAllowScroll', value);
-    //     this.mListViewAllowScroll = value;
-    // }
-    // bottomSheetCanPan() {
-    //     console.log('bottomSheetCanPan', this.listViewAllowScroll);
-    //     return this.listViewAllowScroll;
-    // }
-    gestureHandler;
-    tapGestureHandler;
-    doubleGestureHandler;
-    mounted() {
-        super.mounted();
+    swipeGestureHandler
+    nativeGestureHandler
+    items = [
+            { index: 0, name: 'TURQUOISE', color: '#1abc9c' },
+            { index: 1, name: 'EMERALD', color: '#2ecc71' },
+            { index: 2, name: 'PETER RIVER', color: '#3498db' },
+            { index: 3, name: 'AMETHYST', color: '#9b59b6' },
+            { index: 4, name: 'WET ASPHALT', color: '#34495e' },
+            { index: 5, name: 'GREEN SEA', color: '#16a085' },
+            { index: 6, name: 'NEPHRITIS', color: '#27ae60' },
+            { index: 7, name: 'BELIZE HOLE', color: '#2980b9' },
+            { index: 8, name: 'WISTERIA', color: '#8e44ad' },
+            { index: 9, name: 'MIDNIGHT BLUE', color: '#2c3e50' },
+            { index: 10, name: 'SUN FLOWER', color: '#f1c40f' },
+            { index: 11, name: 'CARROT', color: '#e67e22' },
+            { index: 12, name: 'ALIZARIN', color: '#e74c3c' },
+            { index: 13, name: 'CLOUDS', color: '#ecf0f1' },
+            { index: 14, name: 'CONCRETE', color: '#95a5a6' },
+            { index: 15, name: 'ORANGE', color: '#f39c12' },
+            { index: 16, name: 'PUMPKIN', color: '#d35400' },
+            { index: 17, name: 'POMEGRANATE', color: '#c0392b' },
+            { index: 18, name: 'SILVER', color: '#bdc3c7' },
+            { index: 19, name: 'ASBESTOS', color: '#7f8c8d' }
+        ];
+        initGestures() {
+        const scrollView = (this.$refs.collectionView as any).nativeView;
         const manager = Manager.getInstance();
-        const gestureHandler = (this.gestureHandler = manager.createGestureHandler(HandlerType.PAN, 10, {
-            shouldCancelWhenOutside: false
-        }))
-            .on(GestureHandlerTouchEvent, this.onGestureTouch, this)
-            .on(GestureHandlerStateEvent, this.onGestureState, this);
-        const doubleGestureHandler = (this.doubleGestureHandler = manager.createGestureHandler(HandlerType.TAP, 13, { numberOfTaps: 2 }).on(GestureHandlerStateEvent, this.onDoubleTapGesture, this));
-        const tapGestureHandler = (this.tapGestureHandler = manager
-            .createGestureHandler(HandlerType.TAP, 12, {
-                // waitFor: [13]
-            })
-            .on(GestureHandlerStateEvent, this.onTapGesture, this));
-        // const pinchGestureHandler = (this.tapGestureHandler = manager.createGestureHandler(HandlerType.PINCH, 14).on(GestureHandlerStateEvent, this.onPinchGesture, this));
-
-        gestureHandler.attachToView((this.$refs.subView as any).nativeView);
-        tapGestureHandler.attachToView((this.$refs.subView as any).nativeView);
-        doubleGestureHandler.attachToView((this.$refs.subView as any).nativeView);
-        // pinchGestureHandler.attachToView(this.nativeView);
-    }
-    onDoubleTapGesture(event: GestureStateEventData) {
-        // console.log('onDoubleTapGesture', event.data.extraData, event.data.state, event.data.prevState);
-        if (event.data.state === GestureState.END && event.data.prevState === GestureState.ACTIVE) {
-            console.log('onDoubleTapGesture', event.data.extraData.x, event.data.extraData.y, event.data.extraData);
-        }
-    }
-    onTapGesture(event: GestureStateEventData) {
-        // console.log('onTapGesture', event.data.extraData, event.data.state, event.data.prevState);
-        if (event.data.state === GestureState.END && event.data.prevState === GestureState.ACTIVE) {
-            console.log('onTapGesture', event.data.extraData.x, event.data.extraData.y, event.data.extraData);
-        }
-    }
-    onPinchGesture(event: GestureStateEventData) {
-        // console.log('onTapGesture', event.data.extraData, event.data.state, event.data.prevState);
-        console.log('onPinchGesture', event.data.extraData.positions);
-    }
-    onGestureTouch(args: GestureTouchEventData) {
-        const { state, extraData, view } = args.data;
-        // console.log('onGestureTouch', state, view, extraData.translationX, extraData.translationY);
-        this.translateX = extraData.translationX;
-        this.translateY = extraData.translationY;
+        const gestureHandler = manager.createGestureHandler(HandlerType.FLING, FLING_GESTURE_TAG, {
+            // waitFor: [NATIVE_GESTURE_TAG],
+            simultaneousHandlers: [NATIVE_GESTURE_TAG],
+            direction: FlingDirection.DIRECTION_RIGHT | FlingDirection.DIRECTION_LEFT,
+        });
+        this.swipeGestureHandler = gestureHandler as any;
+        gestureHandler.on(GestureHandlerStateEvent, this.onGestureState, this);
+        gestureHandler.attachToView(this.nativeView);
+        const nativegestureHandler = manager.createGestureHandler(HandlerType.NATIVE_VIEW, NATIVE_GESTURE_TAG, {
+            // shouldActivateOnStart:true,
+            disallowInterruption:true,
+            // waitFor: [FLING_GESTURE_TAG],
+            // simultaneousHandlers: [FLING_GESTURE_TAG],
+            // direction: FlingDirection.DIRECTION_RIGHT | FlingDirection.DIRECTION_LEFT,
+        });
+        // gestureHandler.on(GestureHandlerStateEvent, this.onGestureState, this);
+        nativegestureHandler.attachToView(scrollView);
+        this.nativeGestureHandler = nativegestureHandler as any;
     }
     onGestureState(args: GestureStateEventData) {
         const { state, prevState, extraData, view } = args.data;
-        // console.log('onGestureState', state, prevState, view, extraData, extraData.translationX);
+        if (state === GestureState.ACTIVE) {
+            const direction = extraData.direction as any;
+            console.log('direction', direction, Date.now())
+        }
     }
-    onButtonTap(e) {
-        const { object, view, ...others } = e;
-        console.log('onButtonTap', others);
-        this.bottomSheetHolder.peek();
-    }
+    mounted() {
+        this.initGestures();
 
-    printGestureResult(e) {
-        const { object, view, ...others } = e;
-        console.log('onGesture', others);
     }
-
-    // onItemTap(item) {
-    //     const module = require(`./examples/${item.component}.vue`).default;
-    //     console.log(`Tapped3 on ${item.title}, ${item.component}`, module);
-    //     this.$navigateTo(module, {
-    //         props: {
-    //             title: item.title,
-    //             description: item.description
-    //         }
-    //     } as any);
-    // }
 }
 </script>
 
