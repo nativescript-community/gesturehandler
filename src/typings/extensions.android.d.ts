ca6b2dd9e4315a69dcd3984bc5d8ff764d0e64f2
Martin Guillon <martin.guillon@akylas.fr>
2021-05-12 16:13:20 +0200

    chore: typings

--- src/typings/extensions.android.d.ts
+++ src/typings/extensions.android.d.ts
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-qualifier */
 declare namespace com {
     export namespace nativescript {
         export namespace gesturehandler {
             class GestureHandlerInteractionController extends com.swmansion.gesturehandler.GestureHandlerInteractionController {
                 configureInteractions(handler: com.swmansion.gesturehandler.GestureHandler<any>, waitFor: number[], simultaneousHandlers: number[]);
             }
-            class RootViewGestureHandler extends com.swmansion.gesturehandler.GestureHandler<RootViewGestureHandler> {
-            }
+            class RootViewGestureHandler extends com.swmansion.gesturehandler.GestureHandler<RootViewGestureHandler> {}
             class PageLayout extends org.nativescript.widgets.GridLayout {
+                constructor(context, rootGestureTag);
                 initialize();
                 tearDown();
                 registry(): com.swmansion.gesturehandler.GestureHandlerRegistryImpl;
+                getRootGestureTag(): number;
             }
         }
     }
